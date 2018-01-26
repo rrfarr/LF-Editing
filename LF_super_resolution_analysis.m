0ae615a5c80be1e480f81b4de600fdf4f85facfe
rrfarr <reuben.farrugia@um.edu.mt>
2018-01-26 09:34:54 +0100

    Integrated PCA+RR method

--- LF_super_resolution_analysis.m
+++ LF_super_resolution_analysis.m
@@ -1,9 +1,48 @@
-function LF_super_resolution_analysis()
-% This is the main function suitable to analyse the performance of the
-% proposed light field super-resolution algorithm that computes existing
-% super-resolution techniques on the principal basis. We decompose the
-% light field into a set of basis elements X = BC and we super-resolve the
-% principal basis B.
+function LF_super_resolution_analysis(sr_method,mf,out_flag)
+% The LF_super_resolution_analysis script is a command line script that
+% allows to analyse the performance of a number of light field
+% super-resolution algorithms.
+% 
+% Syntax: LF_super_resolution_analysis('bicubic',3)
+%
+% Input: sr_method: which specifies the super-resolution algorithm to be 
+%                   simulated. The following is a list of sr_methods that 
+%                   are supported here:
+%                   - 'bicubic' - classical bicubic interpolation of each
+%                                 sub-aperture image independently
+%                   - 'lf_srcnn' - this method applies a CNN to restore
+%                                  each sub-ape[rture image separately from 
+%                                  the others [1],[2]
+%                   - 'vdsr' - this method applies the VDSR single image
+%                             super reslution algorithm to restore each
+%                             sub-aperture image separately from the
+%                             others [3]. 
+%                   - 'pca_rr' - this method applies the pca_rr published
+%                                in [4] - super-resolves patch volumes
+%
+%        mf: numeric value that stands for the magnification factor that
+%        the method has to super-resolve
+%
+%        out_flag: This is a boolean value which specifies if the results
+%        attined in the simulation will be stored or not. By default it is
+%        set to false
+%
+% [1] Y. Yoon, H. G. Jeon, D. Yoo, J. Y. Lee and I. S. Kweon, "Light-Field Image 
+% Super-Resolution Using Convolutional Neural Network," in IEEE Signal 
+% Processing Letters, vol. 24, no. 6, pp. 848-852, June 2017.
+%                      
+% [2] C. Dong, C. C. Loy, K. He and X. Tang, "Image Super-Resolution Using 
+% Deep Convolutional Networks," in IEEE Transactions on Pattern Analysis 
+% and Machine Intelligence, vol. 38, no. 2, pp. 295-307, Feb. 1 2016.
+% 
+% [3] J. Kim, J. K. Lee and K. M. Lee, "Accurate Image Super-Resolution Using 
+% Very Deep Convolutional Networks," 2016 IEEE Conference on Computer Vision 
+% and Pattern Recognition (CVPR), Las Vegas, NV, 2016, pp. 1646-1654.
+% 
+% [4] R.A. Farrugia, C. Galea, C. Guillemot, "Super Resolution of Light Field 
+% Images using Linear Subspace Projection of Patch-Volumes," in IEEE 
+% Journal on Selected Topics in Signal Processing, vol. 11, no. 7, 
+% pp. 1058-1071, Oct. 2017
 
 clc; close all;
 
@@ -15,16 +54,12 @@ warning off;
 %--------------------------------------------------------------------------
 % SIMULATION CONFIGURATION
 %--------------------------------------------------------------------------
-sr_method   = 'vdsr';  % Super-resolution methods to consider. For now the 
-                        % simulation will consider 'bicubic', 'srcnn' and 'vdsr'
-                        
-mf = 3;                 % Magnification factor
 
+if nargin == 2
+    out_flag = false;
+end
 % Load the light fields to be processed in this simulation
-[lf_names, datasets] = read_configuration('simulation.cfg');
-
-% Determine the processing method
-proc_method = get_sr_processing_method(sr_method);
+[lf_names, datasets] = read_configuration('config\superresolution.cfg');
 
 if strcmp(sr_method,'srcnn') || strcmp(sr_method,'lf_srcnn')
     addpath('MATLAB/superresolution/srcnn/');
@@ -35,34 +70,43 @@ end
 N = size(lf_names,2);
 
 fprintf('--------------------------------------------------------------\n');
-if strcmp(proc_method,'independentSR') && (strcmp(sr_method,'srcnn') || (strcmp(sr_method,'lf_srcnn')))
+if (strcmp(sr_method,'srcnn') || (strcmp(sr_method,'lf_srcnn')))
     fprintf('Evaluating the performance of SRCNN\n');
     out_filename = sprintf('RESULTS/superresolution/x%d/srcnn.csv',mf);
     out_img_foldername = sprintf('RESULTS/superresolution/x%d/centre_view/srcnn/',mf);
     out_LF_foldername  = sprintf('RESULTS/superresolution/x%d/LF/srcnn/',mf);
-elseif strcmp(proc_method,'independentSR') && strcmp(sr_method,'bicubic')
+elseif strcmp(sr_method,'bicubic')
     fprintf('Evaluating the performance of Bicubic interpolation\n');
     out_filename = sprintf('RESULTS/superresolution/x%d/bicubic.csv',mf);
     out_img_foldername = sprintf('RESULTS/superresolution/x%d/centre_view/bicubic/',mf);
     out_LF_foldername  = sprintf('RESULTS/superresolution/x%d/LF/bicubic/',mf);
-elseif strcmp(proc_method,'independentSR') && strcmp(sr_method,'vdsr')
+elseif  strcmp(sr_method,'vdsr')
     fprintf('Evaluating the performance of VDSR\n');
     out_filename = sprintf('RESULTS/superresolution/x%d/vdsr.csv',mf);
     out_img_foldername = sprintf('RESULTS/superresolution/x%d/centre_view/vdsr/',mf);
     out_LF_foldername  = sprintf('RESULTS/superresolution/x%d/LF/vdsr/',mf);
+elseif strcmp(sr_method,'pca_rr')
+    fprintf('Evaluating the performance of PCA+RR\n');
+    out_filename = sprintf('RESULTS/superresolution/x%d/pca_rr.csv',mf);
+    out_img_foldername = sprintf('RESULTS/superresolution/x%d/center_view/pca_rr/',mf);
+    out_LF_foldername = sprintf('RESULTS/superresolution/x%d/LF/pca_rr/',mf);
 end
 fprintf('--------------------------------------------------------------\n');
 
+% Make sure that the folder containing the images is available
 if ~exist(out_img_foldername,'dir')
     mkdir(out_img_foldername);
 end
 
+% Make sure that the folder containing the light fields is available
 if ~exist(out_LF_foldername,'dir')
     mkdir(out_LF_foldername);
 end
 
 % Open the file where the results will be stored
-fid = fopen(out_filename,'w');
+if out_flag
+    fid = fopen(out_filename,'w');
+end
 
 for n = 1:N
     % Get the dataset
@@ -94,34 +138,40 @@ for n = 1:N
     % Generate the low-resolution light field
     LR_LF = lf_downsample(HR_LF,mf);
     
-    if strcmp(proc_method,'independentSR')
-        % The light field will be restored one sub-aperture image at a time
-        % and will not exploit the light field structure
-        if strcmp(sr_method,'bicubic')
-            SR_LF = lf_bicubic(LR_LF);
-        elseif strcmp(sr_method,'srcnn') || strcmp(sr_method,'lf_srcnn')
-            SR_LF = lf_srcnn(LR_LF,mf);
-        elseif strcmp(sr_method,'vdsr')
-            SR_LF = lf_vdsr(LR_LF,mf);
-        end
-            
-    elseif strcmp(proc_method,'coherentSR')
+    % The light field will be restored one sub-aperture image at a time
+    % and will not exploit the light field structure
+    if strcmp(sr_method,'bicubic')
+        % Bicubic interpolation of each sub-aperture image separately
+        SR_LF = lf_bicubic(LR_LF);
+    elseif strcmp(sr_method,'srcnn') || strcmp(sr_method,'lf_srcnn')
+        % Super-resolution using LF-SRCNN[1],[2]
+        SR_LF = lf_srcnn(LR_LF,mf);
+    elseif strcmp(sr_method,'vdsr')
+        % Super-resolution using VDSR on each sub-aperture image [3]
+        SR_LF = lf_vdsr(LR_LF,mf);
+    elseif strcmp(sr_method,'pca_rr')
+        % Super-resolution using PCA+RR [4]
+        SR_LF = pca_rr_parent(LR_LF,mf);
     end
-    
+            
     % Extract the centre view
     Ic = SR_LF(:,:,:,5,5);
     
-    % Write the center view
-    imwrite(Ic,out_img_filename);
+    if out_flag
+        % Write the center view
+        imwrite(Ic,out_img_filename);
+    end
     
     % Compute the psnr evaluation
     psnr_val = quality_analysis(HR_LF, SR_LF);
     
     fprintf('%s, %s, %0.4f\n',lf_name,dataset,psnr_val);
-    fprintf(fid,'%s, %s, %0.4f\n',lf_name,dataset,psnr_val);
+    if out_flag
+        fprintf(fid,'%s, %s, %0.4f\n',lf_name,dataset,psnr_val);
     
-    % Save the restored light field
-    save(out_LF_filename,'SR_LF');
+        % Save the restored light field
+        save(out_LF_filename,'SR_LF');
+    end
 end
 fclose('all');
 
